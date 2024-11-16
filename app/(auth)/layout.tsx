aaab2f4673fcebe2f5c147b66954c7173f752210
John Doe <devonly1984@hotmail.com>
2024-11-16 03:13:34 -0600

    Added prebuilt files

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+const AuthLayout = ({
+    children,
+  }: Readonly<{
+    children: ReactNode;
+  }>) => {
+  return <div>{children}</div>;
+};
+export default AuthLayout;
---
+++ lib/appwrite/config.ts
@@ -0,0 +1,8 @@
+export const AppwriteConfig = {
+  urlEndpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT,
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+  usersCollection: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION,
+  filesCollection: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION,
+  bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET,
+};
